a4c2407930dd98ec387ca7bfe893aa2547d4e902
Vibemotions <384005485@qq.com>
2023-03-08 19:37:07 +0800

    add exception

--- aceconn/exception.h
+++ aceconn/exception.h
@@ -11,13 +11,16 @@ protected:
     const int errNo;
 
 public:
-    SQLException(const SQLException &e) : std::runtime_error(e.what()), sql_state(e.sql_state), errNo(e.errNo) {}
+    SQLException(const SQLException &e) : std::runtime_error(e.what()), sql_state(e.sql_state), errNo(e.errNo) { }
 
-    SQLException(const std::string& reason, const std::string& SQLState) : std::runtime_error(reason), sql_state(SQLState), errNo(0) {}
+    SQLException(const std::string& reason, const std::string& SQLState, int vendorCode) :
+        std::runtime_error(reason), sql_state(SQLState), errNo(vendorCode) { }
 
-    SQLException(const std::string& reason) : std::runtime_error(reason), sql_state("HY000"), errNo(0) {}
+    SQLException(const std::string& reason, const std::string& SQLState) : std::runtime_error(reason), sql_state(SQLState), errNo(0) { }
 
-    SQLException() : std::runtime_error(""), sql_state("HY000"), errNo(0) {}
+    SQLException(const std::string& reason) : std::runtime_error(reason), sql_state("HY000"), errNo(0) { }
+
+    SQLException() : std::runtime_error(""), sql_state("HY000"), errNo(0) { }
 
     const std::string & getSQLState() const {
         return sql_state;
@@ -33,5 +36,38 @@ public:
 
     virtual ~SQLException() noexcept {};
 };
+
+struct MethodNotImplementedException : public SQLException
+{
+    MethodNotImplementedException(const MethodNotImplementedException& e) : SQLException(e.what(), e.sql_state, e.errNo) { }
+    MethodNotImplementedException(const std::string& reason) : SQLException(reason, "", 0) { }
+};
+
+struct InvalidArgumentException : public SQLException
+{
+    InvalidArgumentException(const InvalidArgumentException& e) : SQLException(e.what(), e.sql_state, e.errNo) { }
+    InvalidArgumentException(const std::string& reason) : SQLException(reason, "", 0) { }
+};
+
+struct InvalidInstanceException : public SQLException
+{
+
+};
+
+struct NonScrollableException : public SQLException
+{
+
+};
+
+struct SQLUnsupportedOptionException : public SQLException
+{
+    SQLUnsupportedOptionException() {}
+
+    ~SQLUnsupportedOptionException() noexcept;
+
+protected:
+    const std::string option;
+};
+
 
 #endif
