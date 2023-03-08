5bc0673a4712677527444521c3caa35bb8d8cae9
Vibemotions <384005485@qq.com>
2023-03-08 18:39:23 +0800

    add exception

---
+++ aceconn/exception.h
@@ -0,0 +1,37 @@
+#ifndef _EXCEPTION_H_
+#define _EXCEPTION_H_
+
+#include <stdexcept>
+#include <string>
+#include <memory>
+
+class SQLException : public std::runtime_error {
+protected:
+    const std::string sql_state;
+    const int errNo;
+
+public:
+    SQLException(const SQLException &e) : std::runtime_error(e.what()), sql_state(e.sql_state), errNo(e.errNo) {}
+
+    SQLException(const std::string& reason, const std::string& SQLState) : std::runtime_error(reason), sql_state(SQLState), errNo(0) {}
+
+    SQLException(const std::string& reason) : std::runtime_error(reason), sql_state("HY000"), errNo(0) {}
+
+    SQLException() : std::runtime_error(""), sql_state("HY000"), errNo(0) {}
+
+    const std::string & getSQLState() const {
+        return sql_state;
+    }
+
+    const char* getSQLStateCStr() const {
+        return sql_state.c_str();
+    }
+
+    int getErrorCode() const {
+        return errNo;
+    }
+
+    virtual ~SQLException() noexcept {};
+};
+
+#endif
