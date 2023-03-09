ff4f42b7e2f348e76fe33a2ad7b6aee96875f8a7
Vibemotions <384005485@qq.com>
2023-03-09 15:20:57 +0800

    add exception.h

--- aceconn/exception.h
+++ aceconn/exception.h
@@ -1,22 +1,30 @@
-#ifndef _EXCEPTION_H_
-#define _EXCEPTION_H_
+#ifndef _SQL_EXCEPTION_H_
+#define _SQL_EXCEPTION_H_
 
 #include <stdexcept>
 #include <string>
 #include <memory>
 
 class SQLException : public std::runtime_error {
+
 protected:
     const std::string sql_state;
     const int errNo;
 
 public:
-    SQLException(const SQLException &e) : std::runtime_error(e.what()), sql_state(e.sql_state), errNo(e.errNo) {}
+    SQLException(const SQLException& e) : std::runtime_error(e.what()), sql_state(e.sql_state), errNo(e.errNo) {}
 
     SQLException(const std::string& reason, const std::string& SQLState, int vendorCode) :
-        std::runtime_error(reason), sql_state(SQLState), errNo(vendorCode) {}
+        std::runtime_error(reason),
+        sql_state(SQLState),
+        errNo(vendorCode)
+    {}
 
-    SQLException(const std::string& reason, const std::string& SQLState) : std::runtime_error(reason), sql_state(SQLState), errNo(0) {}
+    SQLException(const std::string& reason, const std::string& SQLState) :
+        std::runtime_error(reason),
+        sql_state(SQLState),
+        errNo(0)
+    {}
 
     SQLException(const std::string& reason) : std::runtime_error(reason), sql_state("HY000"), errNo(0) {}
 
