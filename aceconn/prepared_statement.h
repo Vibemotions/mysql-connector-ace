90dc53111f8b2fd684c053e64194d2a2b0a11c85
Vibemotions <384005485@qq.com>
2023-03-09 19:55:30 +0800

    add prepared_statement.h

--- aceconn/prepared_statement.h
+++ aceconn/prepared_statement.h
@@ -18,6 +18,18 @@ public:
 
     virtual bool execute(const SQLString& sql) = 0;
     virtual bool execute() = 0;
+
+    virtual ResultSet *executeQuery(const SQLString& sql) = 0;
+    virtual ResultSet *executeQuery() = 0;
+
+    virtual int executeUpdate(const SQLString& sql) = 0;
+    virtual int executeUpdate() = 0;
+
+    virtual ParameterMetaData *getParameterMetaData() = 0;
+
+    virtual void setInt(unsigned int parameterIndex, int32_t value) = 0;
+
+    virtual void setString(unsigned int parameterIndex, SQLString& value) = 0;
 };
 
 #endif
