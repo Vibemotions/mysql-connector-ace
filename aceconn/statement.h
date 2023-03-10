03ad4b7a26a22a8314b2fd6274d6a6a5e32c0769
Vibemotions <384005485@qq.com>
2023-03-10 11:22:15 +0800

    update statement.h

--- aceconn/statement.h
+++ aceconn/statement.h
@@ -2,6 +2,7 @@
 #define _SQL_STATEMENT_H_
 
 #include <string>
+
 #include "resultset.h"
 
 class ResultSet;
@@ -13,6 +14,8 @@ class Statement {
 public:
     virtual ~Statement() {};
 
+    virtual Connection *getConnection() = 0;
+
     virtual void close() = 0;
 
     virtual bool execute(const SQLString& sql) = 0;
@@ -23,6 +26,8 @@ public:
 
     virtual ResultSet *getResultSet() = 0;
 
+    virtual SQLWarning *getWarnings() = 0;
+
     virtual void setCursorName(const SQLString& name) = 0;
 
     virtual void setMaxRows(unsigned int max) = 0;
@@ -30,9 +35,7 @@ public:
     virtual void setQueryTimeout(unsigned int seconds) = 0;
 
     virtual int setQueryAttrInt(const SQLString& name, int32_t value) = 0;
-
     virtual int setQueryAttrDouble(const SQLString &name, double value) = 0;
-
     virtual int setQueryAttrNull(const SQLString &name) = 0;
 
     virtual void clearAttributes() = 0;
