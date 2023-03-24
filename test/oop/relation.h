3736a61e59cd68b0035759df88dbc503d3c99f41
Vibemotions <384005485@qq.com>
2023-03-24 16:20:36 +0800

    add relation.h

---
+++ test/oop/relation.h
@@ -0,0 +1,16 @@
+#ifndef __RELATION__
+#define __RELATION__
+
+using namespace std;
+
+class Base
+{
+
+};
+
+class Derived
+{
+
+};
+
+#endif  // __RELATION__
---
+++ test/oop/string.h
@@ -0,0 +1,16 @@
+#ifndef __STRING__
+#define __STRING__
+
+#include<iostream>
+
+using namespace std;
+
+class String
+{
+public:
+
+private:
+    char* m_data;
+};
+
+#endif  // __STRING__
