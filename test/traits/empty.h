e4f6f6daece191f75ab1fca5499bafcf99fc03b7
Vibemotions <384005485@qq.com>
2023-03-28 13:25:19 +0800

    add test_detection()

--- test/traits/empty.h
+++ test/traits/empty.h
@@ -1,9 +1,28 @@
 #ifndef __EMPTY__
 #define __EMPTY__
 
-class Empty
-{
+class Empty {
+    /*
+        '= default' can only appear on Big Five, which includes
+        0. ctor
+        1. copy ctor
+        2. move ctor
+        3. copy operator=
+        4. move operator=
+        5. dtor
+    
+        '= delete' can be used for any functions
+    */
+};
 
+// implementation of singleton
+class NoCopy
+{
+public:
+    NoCopy() = default;
+    NoCopy(const NoCopy&) = delete;
+    NoCopy& operator = (const NoCopy&) = delete;
+    ~NoCopy() = default;
 };
 
 #endif  // __EMPTY__
--- test/traits/main.cpp
+++ test/traits/main.cpp
@@ -3,6 +3,17 @@
 
 using namespace std;
 
+void test_empty() {
+    Empty e1;
+    Empty e2(e1);
+    e2 = e1;
+}
+
+void test_detection() {
+
+}
+
 int main() {
-    ;
+    // test_empty();
+    test_detection();
 }
