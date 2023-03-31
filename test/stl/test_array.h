9335233a6cd58ff2ad7270be66972c3d5d06b307
Vibemotions <384005485@qq.com>
2023-03-31 15:15:23 +0800

    add qsort to test_array()

--- test/stl/test_array.h
+++ test/stl/test_array.h
@@ -10,6 +10,10 @@
 using namespace std;
 
 const long ASIZE = 500000L;
+
+int compareLongs(const void* a, const void* b) {
+    return (*(long*)a - *(long*)b);
+}
 namespace bb01
 {
 
@@ -21,6 +25,13 @@ void test_array() {
     for (long i = 0; i < ASIZE; ++i) { c[i] = rand(); }
     cout << "milli-seconds : " << clock() - timeStart << endl;
     cout << "array.size() = : " << c.size() << endl;
+    cout << "array.front() = " << c.front() << endl;
+    cout << "array.back() = " << c.back() << endl;
+
+    timeStart = clock();
+    // sort(c.begin(), c.end());
+    ::qsort(c.data(), ASIZE, sizeof(long), compareLongs);
+    cout << "calling sort...\nmilli-seconds : " << clock() - timeStart << endl;
     cout << "array.front() = " << c.front() << endl;
     cout << "array.back() = " << c.back() << endl;
     cout << "array.data() = " << c.data() << endl;
