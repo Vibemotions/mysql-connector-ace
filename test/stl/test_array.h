1fad89400e8b83905a0bc227e60d07b300ca9e06
Vibemotions <384005485@qq.com>
2023-03-31 14:23:22 +0800

    add test_array()

--- test/stl/test_array.h
+++ test/stl/test_array.h
@@ -5,14 +5,25 @@
 #include <iostream>
 #include <stdexcept>
 #include <algorithm>
+#include <array>
 
 using namespace std;
 
+const long ASIZE = 500000L;
 namespace bb01
 {
 
 void test_array() {
-    cout << "this is array" << endl;
+    cout << "test_array()......" << endl;
+    array<long, ASIZE> c;
+    clock_t timeStart = clock();
+    
+    for (long i = 0; i < ASIZE; ++i) { c[i] = rand(); }
+    cout << "milli-seconds : " << clock() - timeStart << endl;
+    cout << "array.size() = : " << c.size() << endl;
+    cout << "array.front() = " << c.front() << endl;
+    cout << "array.back() = " << c.back() << endl;
+    cout << "array.data() = " << c.data() << endl;
 }
 
 }
