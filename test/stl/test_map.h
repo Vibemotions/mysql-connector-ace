2086b029e414096583bc22c1233dae5393e0ea18
Vibemotions <384005485@qq.com>
2023-04-04 11:43:28 +0800

    add test_map()

--- test/stl/test_map.h
+++ test/stl/test_map.h
@@ -5,15 +5,45 @@
 #include <iostream>
 #include <stdexcept>
 #include <algorithm>
-
+#include <map>
+#include "common.h"
 
 using namespace std;
 
-namespace bb01
+namespace bb11
 {
 
-void test_array() {
-    cout << "this is array" << endl;
+const int MAP_SIZE = 1000000;
+const int RANGE = 100000;
+
+void test_map() {
+    cout << "test_map()......" << endl;
+    map<int, int> c;
+    clock_t timeStart = clock();
+
+    for (long i = 0; i < MAP_SIZE; ++i) {
+        try {
+            c[i] = rand() % RANGE;
+        } catch(exception& e) {
+            cout << "i = " << i << " " << e.what() << endl;
+            abort();
+        }
+    }
+    cout << "milli-seconds : " << clock() - timeStart << endl;
+    cout << "map.max_size() = : " << c.max_size() << endl;
+    cout << "map.size() = : " << c.size() << endl;
+    
+    timeStart = clock();
+    auto it = c.find(target);
+    cout << "calling map.find...\nmilli-seconds : " << clock() - timeStart << endl;
+    if (it != c.end()) {
+        cout << "FOUND " << target << endl;
+    } else {
+        cout << "NOT FOUND" << endl;
+    }
+
+    cout << endl;
+    c.clear();
 }
 
 }
