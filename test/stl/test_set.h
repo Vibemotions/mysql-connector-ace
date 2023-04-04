1ad93440840ece8d2a1605e044af43b9e4d24017
Vibemotions <384005485@qq.com>
2023-04-04 11:31:40 +0800

    add test_unordered_multimap() test_set()

--- test/stl/test_set.h
+++ test/stl/test_set.h
@@ -5,15 +5,54 @@
 #include <iostream>
 #include <stdexcept>
 #include <algorithm>
-
+#include <set>
+#include "common.h"
 
 using namespace std;
 
-namespace bb01
+namespace bb10
 {
 
-void test_array() {
-    cout << "this is array" << endl;
+const int SET_SIZE = 1000000;
+const int RANGE = 100000;
+
+void test_set() {
+    cout << "test_set()......" << endl;
+    set<int> c;
+    clock_t timeStart = clock();
+
+    for (long i = 0; i < SET_SIZE; ++i) {
+        try {
+            c.insert(rand() % RANGE);
+        } catch(exception& e) {
+            cout << "i = " << i << " " << e.what() << endl;
+            abort();
+        }
+    }
+    cout << "milli-seconds : " << clock() - timeStart << endl;
+    cout << "set.max_size() = : " << c.max_size() << endl;
+    cout << "set.size() = : " << c.size() << endl;
+
+    timeStart = clock();
+    auto it = find(c.begin(), c.end(), target);
+    cout << "calling ::find...\nmilli-seconds : " << clock() - timeStart << endl;
+    if (it != c.end()) {
+        cout << "FOUND " << target << endl;
+    } else {
+        cout << "NOT FOUND" << endl;
+    }
+
+    timeStart = clock();
+    auto pItem = c.find(target);
+    cout << "calling set.find...\nmilli-seconds : " << clock() - timeStart << endl;
+    if (pItem != c.end()) {
+        cout << "FOUND " << target << endl;
+    } else {
+        cout << "NOT FOUND" << endl;
+    }
+
+    cout << endl;
+    c.clear();
 }
 
 }
--- test/stl/test_unordered_multimap.h
+++ test/stl/test_unordered_multimap.h
@@ -5,15 +5,45 @@
 #include <iostream>
 #include <stdexcept>
 #include <algorithm>
-
+#include <unordered_map>
+#include "common.h"
 
 using namespace std;
 
-namespace bb01
+namespace bb09
 {
 
-void test_array() {
-    cout << "this is array" << endl;
+const int UNORDERED_MULTIMAP_SIZE = 1000000;
+const int RANGE = 100000;
+
+void test_unordered_multimap() {
+    cout << "test_unordered_multimap()......" << endl;
+    unordered_multimap<int, int> c;
+    clock_t timeStart = clock();
+
+    for (int i = 0; i < UNORDERED_MULTIMAP_SIZE; ++i) {
+        try {
+            c.insert(pair<int, int>(i, rand() % RANGE));
+        } catch (exception& e) {
+            cout << "i = " << i << " " << e.what() << endl;
+            abort();
+        }
+    }
+    cout << "milli-seconds : " << clock() - timeStart << endl;
+    cout << "unordered_multimap.max_size() = " << c.max_size() << endl;
+    cout << "unordered_multimap.size() = " << c.size() << endl;
+
+    timeStart = clock();
+    auto it = c.find(target);
+    cout << "calling unordered_multimap.find...\nmilli-seconds : " << clock() - timeStart << endl;
+    if (it != c.end()) {
+        cout << "FOUND " << target << endl;
+    } else {
+        cout << "NOT FOUND" << endl;
+    }
+
+    cout << endl;
+    c.clear();
 }
 
 }
