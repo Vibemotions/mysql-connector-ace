4ca5b06ffb319497834c178e586ab2c68eec49dc
Vibemotions <384005485@qq.com>
2023-04-04 14:26:09 +0800

    add test_unordered_map() test_stack()

--- test/stl/test_stack.h
+++ test/stl/test_stack.h
@@ -5,15 +5,42 @@
 #include <iostream>
 #include <stdexcept>
 #include <algorithm>
-
+#include <stack>
+#include <list>
+#include <vector>
 
 using namespace std;
 
-namespace bb01
+namespace bb14
 {
 
-void test_array() {
-    cout << "this is array" << endl;
+const int STACK_SIZE = 1000000;
+const int RANGE = 100000;
+
+void test_stack() {
+    cout << "test_stack()......" << endl;
+    stack<int> c;
+    clock_t timeStart = clock();
+
+    for (int i = 0; i < STACK_SIZE; ++i) {
+        try {
+            c.push(rand() % RANGE);
+        } catch (exception& e) {
+            cout << "i = " << i << " " << e.what() << endl;
+            abort();
+        }
+    }
+    cout << "milli-seconds : " << clock() - timeStart << endl;
+    cout << "stack.size() = " << c.size() << endl;
+    cout << "stack.top() = " << c.top() << endl;
+    c.pop();
+    cout << "stack.size() = " << c.size() << endl;
+    cout << "stack.top() = " << c.top() << endl;
+
+    stack<int, list<int>> c1;
+    stack<int, vector<int>> c2;
+
+    cout << endl;
 }
 
 }
--- test/stl/test_unordered_map.h
+++ test/stl/test_unordered_map.h
@@ -5,15 +5,45 @@
 #include <iostream>
 #include <stdexcept>
 #include <algorithm>
-
+#include <unordered_map>
+#include "common.h"
 
 using namespace std;
 
-namespace bb01
+namespace bb13
 {
 
-void test_array() {
-    cout << "this is array" << endl;
+const int UNORDERED_MAP_SIZE = 1000000;
+const int RANGE = 100000;
+
+void test_unordered_map() {
+    cout << "test_unordered_map()......" << endl;
+    unordered_map<int, int> c;
+    clock_t timeStart = clock();
+
+    for (int i = 0; i < UNORDERED_MAP_SIZE; ++i) {
+        try {
+            c.insert(pair<int, int>(i, rand() % RANGE));
+        } catch (exception& e) {
+            cout << "i = " << i << " " << e.what() << endl;
+            abort();
+        }
+    }
+    cout << "milli-seconds : " << clock() - timeStart << endl;
+    cout << "unordered_map.max_size() = " << c.max_size() << endl;
+    cout << "unordered_map.size() = " << c.size() << endl;
+
+    timeStart = clock();
+    auto it = c.find(target);
+    cout << "calling unordered_map.find...\nmilli-seconds : " << clock() - timeStart << endl;
+    if (it != c.end()) {
+        cout << "FOUND " << target << endl;
+    } else {
+        cout << "NOT FOUND" << endl;
+    }
+
+    cout << endl;
+    c.clear();
 }
 
 }
