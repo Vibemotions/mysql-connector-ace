a8209bee9248749b73805e3ca675776ff1e16074
Vibemotions <384005485@qq.com>
2023-04-04 14:42:01 +0800

    add test_queue()

--- test/stl/test_queue.h
+++ test/stl/test_queue.h
@@ -5,15 +5,43 @@
 #include <iostream>
 #include <stdexcept>
 #include <algorithm>
-
+#include <queue>
+#include <list>
+#include <vector>
 
 using namespace std;
 
-namespace bb01
+namespace bb15
 {
 
-void test_array() {
-    cout << "this is array" << endl;
+const int QUEUE_SIZE = 1000000;
+const int RANGE = 100000;
+
+void test_queue() {
+    cout << "test_queue()......" << endl;
+    queue<int> c;
+    clock_t timeStart = clock();
+
+    for (int i = 0; i < QUEUE_SIZE; ++i) {
+        try {
+            c.push(rand() % RANGE);
+        } catch (exception& e) {
+            cout << "i = " << i << " " << e.what() << endl;
+            abort();
+        }
+    }
+    cout << "milli-seconds : " << clock() - timeStart << endl;
+    cout << "queue.size() = " << c.size() << endl;
+    cout << "queue.front() = " << c.front() << endl;
+    cout << "queue.bakc()" << c.back() << endl;
+    c.pop();
+    cout << "queue.size() = " << c.size() << endl;
+    cout << "queue.front() = " << c.front() << endl;
+    cout << "queue.back()" << c.back() << endl;
+
+    queue<int, list<int>> c1;
+    queue<int, vector<int>> c2;
+    cout << endl;
 }
 
 }
