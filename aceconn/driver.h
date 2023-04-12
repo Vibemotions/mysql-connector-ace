fdf1852207e17391a29348cc42fd59773f3a61c4
Vibemotions <384005485@qq.com>
2023-04-12 11:46:19 +0800

    add member func to class Driver

--- aceconn/driver.h
+++ aceconn/driver.h
@@ -9,25 +9,38 @@ protected:
     virtual ~Driver() {}
 
 public:
-    virtual Connection *connect(SQLString& hostName,
+    virtual Connection* connect(SQLString& hostName,
                                 SQLString& userName,
                                 SQLString& password) = 0;
 
-    virtual Connection *connect(ConnectOptionsMap& options) = 0;
+    virtual Connection* connect(ConnectOptionsMap& options) = 0;
 
     virtual int getMajorVersion() = 0;
     virtual int getMinorVersion() = 0;
     virtual int getPatchVersion() = 0;
 
-    virtual const SQLString &getName() = 0;
+    virtual const SQLString& getName() = 0;
 };
+
+void check(const std::string&);
+void check(const std::map<std::string, std::string>&);
+
+inline static void check_lib() {
+    check(std::string {});
+    check(std::map<std::string, std::string> {});
+}
 
 extern "C"
 {
-    Driver *_get_driver_instance_by_name(const char* const clientlib);
+    Driver* _get_driver_instance_by_name(const char* const clientlib);
 
-    inline static Driver* get_driver_instance() {
-        return _get_driver_instance_by_name("");
+    inline static Driver* get_driver_instance_by_name(const char* const clientlib) {
+        check_lib();
+        return _get_driver_instance_by_name(clientlib);
+    }
+
+    inline static Driver *get_driver_instance() {
+        return get_driver_instance_by_name("");
     }
 }
 
