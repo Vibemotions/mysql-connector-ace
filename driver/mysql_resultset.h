4b3f70c0ab317c053c65df6afd4bbbfa5c2c2c13
Vibemotions <384005485@qq.com>
2023-04-13 13:29:57 +0800

    add class mysql_resultset derived from class resultset

--- driver/mysql_resultset.h
+++ driver/mysql_resultset.h
@@ -4,7 +4,33 @@
 #include <ace/Auto_Ptr.h>
 #include <ace/Refcounted_Auto_Ptr.h>
 #include <ace/Bound_Ptr.h>
-#include <cppconn/resultset.h>
+#include <aceconn/resultset.h>
+#include <driver/common.h>
 
+class MySQL_Statement;
+class MySQL_DebugLogger;
+class MySQL_ResultSetMetaData;
+
+class ConnectionWrapper;
+class ResultSetWrapper;
+
+class MySQL_ResultSet : public ResultSet {
+    MYSQL_ROW row;
+    ACE_Refcounted_Auto_Ptr<ResultSetWrapper, ACE_Lock> result;
+    ACE_Weak_Bound_Ptr<ConnectionWrapper, ACE_Lock> proxy;
+    unsigned int num_fields;
+    uint64_t num_rows;
+    uint64_t row_pos;
+    /*
+        0 : before the first row
+        1 : the first row
+        num_rows + 1 : after the last row
+    */
+
+   typedef std::map<SQLString, unsigned int> FieldNameIndexMap;
+   FieldNameIndexMap field_name_to_index_map;
+
+   mutable bool was_null;
+};
 
 #endif  // _MYSQL_RESULTSET_H_
