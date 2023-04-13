80d60db6df911beef394679cbfab847783276f7b
Vibemotions <384005485@qq.com>
2023-04-13 17:09:16 +0800

    add mysql_field

--- driver/common.h
+++ driver/common.h
@@ -1,5 +1,18 @@
 typedef struct MYSQL_FIELD {
-    /* data */
+    char* name;
+    char* table;
+    char* db;
+    char* defvalue;
+    unsigned long length;
+    unsigned long max_length;
+    unsigned int name_length;
+    unsigned int table_length;
+    unsigned int db_length;
+    unsigned int defvalue_length;
+    unsigned int flags;
+    unsigned int decimals;
+    unsigned int charset;
+    void* extension;
 };
 
 typedef char** MYSQL_ROW;
