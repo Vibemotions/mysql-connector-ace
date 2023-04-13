41af1a516ac58691c41bae93412f91f0ba52deeb
Vibemotions <384005485@qq.com>
2023-04-13 14:33:50 +0800

    add mysql_metadata.h

--- driver/mysql_metadata.h
+++ driver/mysql_metadata.h
@@ -23,6 +23,49 @@ class MySQL_MetaData : public DatabaseMetaData {
     ACE_Refcounted_Auto_Ptr<MySQL_DebugLogger, ACE_Lock> logger;
     ACE_Refcounted_Auto_Ptr<ConnectionWrapper, ACE_Lock> proxy;
     unsigned long server_version;
+
+public:
+    MySQL_MetaData(Statement* const stmt,
+                   ACE_Refcounted_Auto_Ptr<ConnectionWrapper, ACE_Lock> capi,
+                   ACE_Refcounted_Auto_Ptr<MySQL_DebugLogger, ACE_Lock>& logger);
+    
+    virtual ~MySQL_MetaData();
+
+    Connection* getConnection();
+
+    SQLString getURL();
+
+    SQLString getUserName();
+
+    ResultSet* getSchemas();
+
+    unsigned int getDatabaseVersion();
+
+    unsigned int getDriverVersion();
+
+    SQLString& getDriverName();
+
+    unsigned getMaxConnections();
+
+    ResultSet* getTables(const SQLString& catalog,
+                                 const SQLString& schema,
+                                 const SQLString& tableName,
+                                 std::list<SQLString>& types);
+
+    ResultSet* getPrimaryKeys(const SQLString& catalog,
+                                      const SQLString& schema,
+                                      const SQLString& table);
+
+    ResultSet* getProcedures(const SQLString& catalog,
+                                     const SQLString& schema,
+                                     const SQLString& procedureName);
+
+    bool isReadOnly();
+
+    bool allProceduresAreCallable();
+
+    bool allTablesAreSelectable();
+
 };
 
 #endif  // _MYSQL_METADATA_H_
