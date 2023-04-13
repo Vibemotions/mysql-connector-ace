7e1064857d310a870cb056f142e72c774b774393
Vibemotions <384005485@qq.com>
2023-04-13 13:46:55 +0800

    add ctor to class mysql_resultset

--- driver/mysql_resultset.h
+++ driver/mysql_resultset.h
@@ -31,6 +31,42 @@ class MySQL_ResultSet : public ResultSet {
    FieldNameIndexMap field_name_to_index_map;
 
    mutable bool was_null;
+
+    // updated by calling ResultSet::getInt() or getDouble() ...
+   mutable uint32_t last_quired_column;
+
+   const MySQL_Statement* stmt;
+
+   ACE_Refcounted_Auto_Ptr<MySQL_DebugLogger, ACE_Lock> logger;
+
+   ACE_Auto_Ptr<MySQL_ResultSetMetaData> rs_meta;
+
+   ResultSet::ResultSetType rs_type;
+
+   unsigned long server_version;
+
+protected:
+    void checkValid() const;
+    void checkSrollable() const;
+    bool isScrollable() const;
+    bool isOutofRange() const;
+    void seek();
+
+    MYSQL_FIELD* getFieldMeta(unsigned int columnIndex) const;
+
+public:
+    MySQL_ResultSet(ACE_Refcounted_Auto_Ptr<ResultSetWrapper, ACE_Lock> res,
+                    ACE_Weak_Bound_Ptr<ConnectionWrapper, ACE_Lock> proxy,
+                    ResultSet::ResultSetType rs_type,
+                    MySQL_Statement* stmt,
+                    ACE_Refcounted_Auto_Ptr<MySQL_DebugLogger, ACE_Lock>& logger);
+
+    virtual ~MySQL_ResultSet();
+
+
+private:
+    MySQL_ResultSet(const MySQL_ResultSet&);
+    void operator=(MySQL_ResultSet&);
 };
 
 #endif  // _MYSQL_RESULTSET_H_
