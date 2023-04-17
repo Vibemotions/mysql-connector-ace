7d024c32585726029103060a081326b86b95f01b
Vibemotions <384005485@qq.com>
2023-04-17 18:16:11 +0800

    add struct host_data

--- driver/mysql_uri.h
+++ driver/mysql_uri.h
@@ -2,6 +2,7 @@
 #define _MYSQL_URI_H_
 
 #include <aceconn/sqlstring.h>
+#include <driver/common.h>
 #include <vector>
 
 class ConnectionWrapper;
@@ -17,17 +18,37 @@ private:
 
 public:
     struct Host_Data {
+        Host_Data(SQLString name, uint16_t port, bool has_port = false) : 
+            name(name),
+            port(port),
+            has_port(has_port),
+            protocol(PROTOCOL_TYPE::PROTOCOL_TCP)
+        {}
 
+        Host_Data();
+
+    private:
+        SQLString name;
+        uint16_t port;
+        bool has_port;
+        PROTOCOL_TYPE protocol;
     };
 
     MySQL_Uri();
 
     /*
-        iterator over Host_list object
+        encapsulate functions of container vector for Host_List
     */
     Host_List::iterator begin() { return host_list.begin(); }
     Host_List::iterator end() { return host_list.end(); }
     size_t size() const { return host_list.size(); }
+
+    void clear() { host_list.clear(); }
+
+    void erase(Host_List::iterator it) { host_list.erase(it); }
+
+    // void addHost(Host_Data host) { host_list.push_back(host); }
+    void setHost(Host_Data host) { ; }
 };
 
 #endif
