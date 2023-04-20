665a16575b02aba9277075ec9d9f1255c7a1749e
Vibemotions <384005485@qq.com>
2023-04-20 18:20:43 +0800

    add class dirverwrapper

--- wrapper/driver_wrapper.h
+++ wrapper/driver_wrapper.h
@@ -2,5 +2,23 @@
 #define _DRIVER_WRAPPER_H_
 
 #include <ace/Refcounted_Auto_Ptr.h>
+#include <aceconn/sqlstring.h>
+
+class MySQLCAPI;
+class ConnectionWrapper;
+
+class DriverWrapper {
+
+public:
+    DriverWrapper(const SQLString& clientFileName);
+    ~DriverWrapper();
+
+    ConnectionWrapper& conn_init();
+
+    DriverWrapper(const DriverWrapper&) = delete;
+    DriverWrapper& operator=(const DriverWrapper&) = delete;
+};
+
+DriverWrapper* createDriverWrapper(const SQLString& clientFileName);
 
 #endif
